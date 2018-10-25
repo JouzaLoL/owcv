02586fbe49ec42ada006bf13fb042418646c91c3
Josef Vacek <vacekj@outlook.com>
2018-10-25 17:13:42 +0200

    debug mode

--- OWCV/CV.cs
+++ OWCV/CV.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Diagnostics;
-using Emgu.CV;
+﻿using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
 using System.Drawing;
-using System.Linq;
 using System.Windows.Forms;
 
 namespace OWCV
@@ -46,18 +43,20 @@ namespace OWCV
         public static bool ShouldFire(VectorOfVectorOfPoint contours, Image<Bgr, byte> drawTarget)
         {
             var centerOfScreen = Cursor.Position;
-            drawTarget.Draw(new CircleF(new PointF(centerOfScreen.X, centerOfScreen.Y), 10), new Bgr(Color.Coral));
+#if DEBUG
+            drawTarget.Draw(new CircleF(new PointF(centerOfScreen.X, centerOfScreen.Y), 5), new Bgr(Color.Coral));
+#endif
 
             for (var i = 0; i < contours.Size; i++)
             {
                 var currObj = contours[i];
+#if DEBUG
                 drawTarget.DrawPolyline(currObj.ToArray(), true, new Bgr(Color.BlueViolet));
+#endif
                 var dist = CvInvoke.PointPolygonTest(currObj, new PointF(centerOfScreen.X, centerOfScreen.Y), true);
 
-                Debug.WriteLine(dist);
                 if (dist > -1)
                 {
-                    Debug.WriteLine(dist);
                     System.Media.SystemSounds.Exclamation.Play();
                     Utility.DoMouseClick();
                     return true;
--- OWCV/Form1.cs
+++ OWCV/Form1.cs
@@ -18,25 +18,29 @@ namespace OWCV
         public Form1()
         {
             InitializeComponent();
-            CvInvoke.NamedWindow("Contours", Emgu.CV.CvEnum.NamedWindowType.FreeRatio);
+#if DEBUG
+            CvInvoke.NamedWindow("Contours", Emgu.CV.CvEnum.NamedWindowType.FreeRatio);            
+#endif
+
 
             var gameWindow = Utility.GetGameWindow();
 
-            var tick = new System.Timers.Timer(100);
+            var tick = new System.Timers.Timer(16);
             tick.Elapsed += (sender, eArgs) =>
             {
                 var bmp = ScreenCapture.CaptureWindow(gameWindow);
                 var source = new Image<Bgr, byte>(bmp);
-                imageBox1.Image = source;
+
                 // FOV
                 var FOV = 150; // px
                 
                 var filtered = CV.FilterRed(source);
                 var thresholded = CV.Threshold(filtered);
                 var canny = CV.Canny(thresholded);
-
                 var contours = CV.Contours(canny, source);
-                CvInvoke.Imshow("Contours", contours);
+#if DEBUG
+                CvInvoke.Imshow("Contours", contours);                
+#endif
             };
 
             tick.Start();
